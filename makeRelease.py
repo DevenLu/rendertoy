ccbb4eea2e8e91b8235253ccfaf33722bf73284a
Tomasz Stachowiak <h3@h3.gd>
2017-02-08 01:30:12 +0100

    removed some unused stuff, updated the build script

--- makeRelease.py
+++ makeRelease.py
@@ -5,11 +5,11 @@ if os.path.exists('release'):
 	shutil.rmtree('release')
 os.mkdir('release')
 
-os.system('tools\\tundra2.exe release')
+os.system('tools\\tundra\\bin-win32\\tundra2.exe release')
 shutil.copy('t2-output/win64-msvc-release-default/rendertoy.exe', 'release')
 shutil.copytree('data', 'release/data')
 shutil.copytree('tools/sublimePlugin', 'release/sublimePlugin')
 
-releaseVersion = '0.1'
+releaseVersion = '0.2'
 
 shutil.make_archive('rendertoy_' + releaseVersion + '_64', 'zip', 'release')
